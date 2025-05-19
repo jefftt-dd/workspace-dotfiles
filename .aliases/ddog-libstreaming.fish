047e7467730ccd57f2b6aa904ca68cc38b393398
jeff.lai <jeff.lai@datadoghq.com>
2025-05-19 18:53:54 +0000

    Add cdddsource

--- .aliases/ddog-libstreaming.fish
+++ .aliases/ddog-libstreaming.fish
@@ -1,5 +1,6 @@
 # Shortcuts for monorepos
 alias cddd='cd $DATADOG_ROOT'
+alias cdddsource='cd $DATADOG_ROOT/dd-source'
 alias cddomains='cd $DATADOG_ROOT/dd-source/domains'
 alias cddomainsstreaming='cd $DATADOG_ROOT/dd-source/domains/streaming'
 alias cdlibstreamingrust='cd $DATADOG_ROOT/dd-source/domains/streaming/shared/libs/libstreaming/rust'
